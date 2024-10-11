81fc05f8880b0ec8c8ab8833b0ef0ff8cf46a5ca
A1ekzfame <68947902+Alekzum@users.noreply.github.com>
2024-10-11 13:27:49 +0400

    Update common.py

--- handlers/common.py
+++ handlers/common.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 import logging
@@ -10,16 +10,21 @@ rt = Router()
 
 
 @rt.message(Command("start"))
-async def cmd_start(message: Message, state: FSMContext, command: list[str]):
+async def cmd_start(message: Message, state: FSMContext, command: CommandObject):
     data = await state.get_data()
     if data is None:
         await state.set_data({})
     
-    match command:
-        case ['start']:
+    match command.args:
+        case []:
             pass
         
         case _:
-            logger.info(f"Args for start are {command}")
+            logger.info(f"Start is {command}")
 
-    await message.answer(f"Hello!")  
+    await message.answer(f"Hello! I am an echo bot! I just forward your messages to you.")
+
+
+@rt.message()
+async def msg_echo(message: Message):
+    await message.forward(message.chat.id, protect_content=True)
