500d50aa9d88e1a85877135b26f6b5d30f21fc4b
Alekzum <68947902+Alekzum@users.noreply.github.com>
2025-02-16 14:21:20 +0400

    Reformat utils.my_routers.include_routers() via pathlib

--- utils/my_routers.py
+++ utils/my_routers.py
@@ -1,7 +1,10 @@
+from importlib import import_module
 from aiogram import Dispatcher
-import os
+import pathlib
 
 
 def include_routers(dp: Dispatcher, root="handlers"):
-    files = [x for x in os.listdir(root) if x.endswith(".py")]
-    dp.include_routers(*[__import__(f'{root}.{file.removesuffix(".py")}', fromlist=('rt')).rt for file in files])
+    files = pathlib.Path(root).glob("*.py")
+    for file in files:
+        module = import_module(".".join((root, file.with_suffix("").name)))
+        dp.include_router(module.rt)
